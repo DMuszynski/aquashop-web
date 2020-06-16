9a6a558aa3ae0b45fcdc1329e38e7e1c11cc9834
DAMIAN_MUSZYNSKI <d.muszynski112@gmail.com>
2020-06-16 22:35:39 +0200

    - first commit

---
+++ aquashop-api/src/main/java/pl/dmuszynski/aquashop/api/AquaShopApplication.java
@@ -0,0 +1,13 @@
+package pl.dmuszynski.aquashop.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class AquaShopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AquaShopApplication.class, args);
+    }
+}
---
+++ aquashop-api/src/test/java/pl/dmuszynski/aquashop/api/AquaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dmuszynski.aquashop.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
