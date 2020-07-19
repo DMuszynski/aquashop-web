a575b040fa702463d6d90cc4014a500b9f42edb9
DAMIAN_MUSZYNSKI <d.muszynski112@gmail.com>
2020-07-19 21:05:21 +0200

    create Category class

--- aquashop-api/src/main/java/pl/dmuszynski/aquashop/model/Category.java
+++ aquashop-api/src/main/java/pl/dmuszynski/aquashop/model/Category.java
@@ -1,4 +1,31 @@
 package pl.dmuszynski.aquashop.model;
 
+import javax.persistence.*;
+
+@Entity
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id", unique = true, nullable = false)
+    private Long id;
+
+    private String name;
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Category() {
+        this(0L, "Category");
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
