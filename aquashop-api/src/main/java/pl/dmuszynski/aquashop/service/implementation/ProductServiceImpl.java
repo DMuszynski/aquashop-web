e08b4222f5b02d7d86eee283f0e1e1f89d1ff14f
DAMIAN_MUSZYNSKI <d.muszynski112@gmail.com>
2020-08-01 14:47:05 +0200

    Create and implements product service implementation class

---
+++ aquashop-api/src/main/java/pl/dmuszynski/aquashop/service/implementation/ProductServiceImpl.java
@@ -0,0 +1,40 @@
+package pl.dmuszynski.aquashop.service.implementation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.dmuszynski.aquashop.repository.ProductRepository;
+import pl.dmuszynski.aquashop.service.ProductService;
+import pl.dmuszynski.aquashop.model.Product;
+
+import javax.transaction.Transactional;
+
+@Service @Transactional
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public void addProduct(Product product) {
+        this.productRepository.save(product);
+    }
+
+    @Override
+    public void updatePrizeById(float prize, Long id) {
+        this.productRepository.updatePrizeById(prize, id);
+    }
+
+    @Override
+    public void updateNameById(String name, Long id) {
+        this.productRepository.updateNameById(name, id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        this.productRepository.deleteById(id);
+    }
+}
