fbd48ea55b7547341ddad4d18675341fa8c043e8
DAMIAN_MUSZYNSKI <d.muszynski112@gmail.com>
2020-08-01 14:38:56 +0200

    Create and implements product service interface

---
+++ aquashop-api/src/main/java/pl/dmuszynski/aquashop/service/ProductService.java
@@ -0,0 +1,10 @@
+package pl.dmuszynski.aquashop.service;
+
+import pl.dmuszynski.aquashop.model.Product;
+
+public interface ProductService {
+    void addProduct(Product product);
+    void updatePrizeById(float prize, Long id);
+    void updateNameById(String name, Long id);
+    void deleteById(Long id);
+}
