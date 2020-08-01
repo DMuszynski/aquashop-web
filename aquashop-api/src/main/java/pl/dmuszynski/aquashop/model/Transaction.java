3090d3070397108557560aa800f39c2f1de5bb48
DAMIAN_MUSZYNSKI <d.muszynski112@gmail.com>
2020-08-01 15:38:26 +0200

    Create and implements transaction entity class

---
+++ aquashop-api/src/main/java/pl/dmuszynski/aquashop/model/Transaction.java
@@ -0,0 +1,55 @@
+package pl.dmuszynski.aquashop.model;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.validation.constraints.NotNull;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "transaction_id", unique = true)
+    private Long id;
+
+    @CreatedDate
+    @NotNull @Column(updatable = false)
+    private LocalDateTime date;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Transaction(Product product, User user, LocalDateTime date) {
+        this.product = product;
+        this.user = user;
+        this.date = date;
+    }
+
+    protected Transaction() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
+
