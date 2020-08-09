a6af076be1d9289d94fd86bdc6a1a393e989cfc5
DAMIAN_MUSZYNSKI <d.muszynski112@gmail.com>
2020-08-09 14:15:10 +0200

    Add payload class implementations

---
+++ aquashop-api/src/main/java/pl/dmuszynski/aquashop/payload/response/MessageResponse.java
@@ -0,0 +1,18 @@
+package pl.dmuszynski.aquashop.payload.response;
+
+public class MessageResponse {
+
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
